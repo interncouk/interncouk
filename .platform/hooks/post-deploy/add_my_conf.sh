86768ae7829e7a156cc488a0cbe4bbd613625816
SkyDev <peakdeveloper1114@gmail.com>
2023-07-23 22:56:19 +0200

    New Script2

--- .platform/hooks/post-deploy/add_my_conf.sh
+++ .platform/hooks/post-deploy/add_my_conf.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+# Print a message before removing the old custom configuration file
+echo "Removing the old custom configuration file..."
 sudo rm /etc/httpd/conf.d/apache_my_custom.conf
+
+# Print a message while creating the new custom configuration file
+echo "Creating the new custom configuration file..."
 sudo cat <<EOF > /etc/httpd/conf.d/elasticbeanstalk/custom.conf
 <Directory /var/www/html>
     Options FollowSymLinks
@@ -7,4 +13,10 @@ sudo cat <<EOF > /etc/httpd/conf.d/elasticbeanstalk/custom.conf
     Require all granted
 </Directory>
 EOF
+
+# Print a message before restarting Apache
+echo "Restarting Apache..."
 sudo systemctl restart httpd
+
+# Print a final message to indicate the end of the script
+echo "Script execution completed."
