843939e76b57ccc23b806275b5359b0028638970
SkyDev <peakdeveloper1114@gmail.com>
2023-07-23 22:27:27 +0200

    New Script 2

--- public/.platform/hooks/post-deploy/add_my_conf.sh
+++ public/.platform/hooks/post-deploy/add_my_conf.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-cp etc/nginx/conf.d/apache_my_custom.conf /etc/httpd/conf.d/elasticbeanstalk/
+sudo rm /etc/httpd/conf.d/apache_my_custom.conf
+sudo cat <<EOF > /etc/httpd/conf.d/elasticbeanstalk/custom.conf
+<Directory /var/www/html>
+    Options FollowSymLinks
+    AllowOverride All
+    Require all granted
+</Directory>
+EOF
 sudo systemctl restart httpd
