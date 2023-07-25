965aff852991ba9652f6bf6647b6ec79c088f688
SkyDev <peakdeveloper1114@gmail.com>
2023-07-25 18:19:17 +0200

    Add new script for nginx configuration

--- platform/plugins/job-board/src/Http/Requests/LoginRequest.php
+++ platform/plugins/job-board/src/Http/Requests/LoginRequest.php
@@ -9,7 +9,6 @@ class LoginRequest extends Request
     public function rules(): array
     {
         return [
-            '_token' => 'required',
             'email' => 'required|string|email',
             'password' => 'required|string',
         ];
